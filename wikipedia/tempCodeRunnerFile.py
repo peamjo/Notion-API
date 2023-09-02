8dbb6624fa11d4c03294ee7d8b3adb338e4e9ec5
peamjo <peam@live.com>
2023-09-02 16:55:55 +0700

    refactor#8

--- wikipedia/tempCodeRunnerFile.py
+++ wikipedia/tempCodeRunnerFile.py
@@ -1,2 +1,13 @@
-    template = front_block + ", " + summary #+ ", " + back_blocks[1:len(back_blocks)-1]
-    print(template)
+name, url, property_name = process_input(movie_name)
+        get_pages(database_id, topic)
+        with open(str(Path.cwd().joinpath(topic +'-db.json')), encoding="utf8") as file:
+            movies_pages = json.loads(file.read())["results"]
+        movie_exist = check_pages(database_id, movies_pages, movie_name, topic)
+        if movie_exist == False:
+            description_data, cast_crew_data = get_movie_info(movie_name, database_id)
+            movie_data, cover_data, icon_data, summary, template = process_movie_info(movie_name, database_id, description_data, cast_crew_data, topic)
+            fast_create_movie(movie_data, cover_data, icon_data, database_id, template)
+            print(f"{movie_name} has been added to the database")
+        else:
+            print(f"{movie_name} already exists in the database")
+    """except KeyboardInterrupt:
